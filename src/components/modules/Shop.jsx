e94c689618ccba42334d1b1e74e5fba077c27809
Szymon Piątkowski <64953895+frageno@users.noreply.github.com>
2022-12-02 16:07:00 +0100

    boilerplate init

---
+++ src/components/modules/Shop.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from '@emotion/styled';
+import tw from 'twin.macro'
+
+const Shop = () => {
+  return (
+    <Test>Shop</Test>
+  )
+}
+
+export default Shop
+
+const Test = styled.div`
+    ${tw`border border-[#000]`};
+    color:pink;
+`
